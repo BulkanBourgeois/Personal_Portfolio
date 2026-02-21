535cf39114e7176addca25fdf2ef29d37531b688
Richard Wang <richardw2006@icloud.com>
2026-02-21 04:02:22 -0600

    Changed scene

--- src/lib/scene/Scene.svelte
+++ src/lib/scene/Scene.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
+	import { interactivity } from '@threlte/extras';
+	import { onDestroy } from 'svelte';
+	import RoomModel from '$lib/scene/models/RoomModel.svelte';
+	import { cameraMode } from '$lib/stores/cameraStore';
+	import { openProject } from '$lib/stores/projectStore';
+
+	interactivity();
+
+	const handleTvClick = () => {
+		cameraMode.set('focus-tv');
+		openProject('portfolio-mvp');
+	};
+
+	const handleTvHoverChange = (isHovering: boolean) => {
+		if (typeof document === 'undefined') return;
+		document.body.style.cursor = isHovering ? 'pointer' : 'default';
+	};
+
+	onDestroy(() => {
+		if (typeof document === 'undefined') return;
+		document.body.style.cursor = 'default';
+	});
 </script>
 
-<T.Mesh>
-	<T.BoxGeometry args={[1, 1, 1]} />
-	<T.MeshBasicMaterial color="hotpink" />
-</T.Mesh>
+<T.PerspectiveCamera makeDefault position={[0, 1.8, 6]} fov={45} />
+<T.AmbientLight intensity={0.8} />
+<T.DirectionalLight position={[4, 6, 4]} intensity={1.1} />
+<RoomModel onTvClick={handleTvClick} onTvHoverChange={handleTvHoverChange} />
